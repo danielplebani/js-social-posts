7bfec05915ee594212a35c6910789d541721b0db
danielplebani <danielpleba03@gmail.com>
2023-09-08 15:24:22 +0200

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,22 @@
+/*
+Descrizione
+Ricreiamo un feed social aggiungendo al layout dello starter kit di base fornito, il nostro script JS in cui:
+
+Milestone 1
+Creiamo il nostro array di oggetti che rappresentano ciascun post.
+Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+-id del post, numero progressivo da 1 a n
+-nome autore,
+-foto autore,
+-data in formato americano (mm-gg-yyyy),
+-testo del post,
+-immagine (non tutti i post devono avere una immagine),
+-numero di likes.
+*/
+const posts =[
+    {
+        id : 1,
+        autore : 'danielplebani',
+        foto : 
+    }
+]
