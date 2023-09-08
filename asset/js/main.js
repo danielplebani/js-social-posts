44291006d490d16bfc880ea2e0b5eb94d612625c
danielplebani <danielpleba03@gmail.com>
2023-09-08 18:48:21 +0200

    counter-like

--- asset/js/main.js
+++ asset/js/main.js
@@ -45,64 +45,80 @@ const posts =[
     }
 ];
 
+const postLiked = [];
+console.log(postLiked);
 
 /*
 Milestone 2
 Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
 */
+
 //CICLO FOR CHE INSERISCE TUTTI I POST CON I DATI IN PAGINA
+
 for (let i = 0; i < posts.length; i++) {
     const post = posts[i];
 
-    function postsMarkup(post) {
-        return `
-          <div class="card w-75 mb-3 py-3 px-2">
-            <div class="d-flex mb-2">
-              <img src="./asset/img/${post.fotoAutore}" class="fotoAutore">
-              <div class="d-flex flex-column">
-                <h5 style="font-size: 18px;" class="card-title m-0">${post.autore}</h5>
-                <span style="font-size: 12px; font-family: 'Oswald', sans-serif;">${post.data}</span>
-              </div>
-            </div>
-            
-            <p class="card-text">${post.testo}</p>
-    
-            <img src="./asset/img/${post.immagine}" class="card-img-top">
-    
-            <div class="card-body p-1 d-flex justify-content-between align-items-center">
-              <i style="color: rgba(128, 128, 128, 0.414);" class="fa-solid fa-heart fs-4 mt-1"></i>
-    
-              <span style="font-size: 17px; font-family: 'Oswald', sans-serif;">piace a ${post.likes} persone</span>
-            </div>
-          </div>
-        `
-    };
-    
-    function addPosts(markup) {
-        const container = document.querySelector('.container');
-        container.insertAdjacentHTML('beforeend', markup);
-    };
     addPosts(postsMarkup(post));
+
+    /*
+    Milestone 3
+    Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+    */
+    const like = document.querySelector('#post_'+ post.id +' i');
+    let isLiked = false;
+
+    like.addEventListener('click', function () {
+    if (isLiked) {
+        like.style.color = 'rgba(128, 128, 128, 0.414)';
+
+        const likeCounter = document.querySelector('#post_'+ post.id +' .likes-counter');
+        likeCounter.innerText = Number(likeCounter.innerText) - 1; 
+        isLiked = false;
+    } else {
+        like.style.color = 'red';
+        postLiked.push(post); 
+
+        const likeCounter = document.querySelector('#post_'+ post.id +' .likes-counter');
+        likeCounter.innerText = Number(likeCounter.innerText) + 1;
+        isLiked = true;
+    }
+});
+
 }
 
+//FUNCTIONS
+/**
+ * Genera HTML del post
+ * @param {Object} post 
+ * @returns String
+ */
+function postsMarkup(post) {
+    return `
+    <div id="post_${post.id}" class="d-flex flex-column align-items-center">
+        <div class="card mb-3 py-3 px-2 w-75">
+        <div class="d-flex mb-2">
+            <img src="./asset/img/${post.fotoAutore}" class="fotoAutore">
+            <div class="d-flex flex-column">
+            <h5 style="font-size: 18px;" class="card-title m-0">${post.autore}</h5>
+            <span style="font-size: 12px; font-family: 'Oswald', sans-serif;">${post.data}</span>
+            </div>
+        </div>
+        
+        <p class="card-text">${post.testo}</p>
 
-/*
-Milestone 3
-Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
-*/
-const like = document.querySelector('i');
+        <img src="./asset/img/${post.immagine}" class="card-img-top">
 
-const postLiked = [];
+        <div class="card-body p-1 d-flex justify-content-between align-items-center">
+            <i style="color: rgba(128, 128, 128, 0.414);" class="fa-solid fa-heart fs-4 mt-1"></i>
 
-like.addEventListener('click', function(){
-    like.style.color = 'red';
+            <span style="font-size: 17px; font-family: 'Oswald', sans-serif;">piace a <span class="likes-counter">${post.likes}</span> persone</span>
+        </div>
+        </div>
+    </div>
+    `
+};
 
-    //posts[key].likes++
-
-    posts.forEach(function (post) {
-        const likeRicevuti = post.likes;
-        likeRicevuti + 1;
-        console.log(likeRicevuti);
-    });
-      
-})
+function addPosts(markup) {
+    const container = document.querySelector('.container');
+    container.insertAdjacentHTML('beforeend', markup);
+};
